f732b764630b4833b5c0eee2b6fc46a0bfa267b4
Matthew Shouse <mshouse8624@gmail.com>
2024-10-09 12:13:54 -0500

    Added index.js file and basic instructions Shouse

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+app = express()
+
+const cors = require("cors")
+app.use(cors({origin: '*'}))
+
+var url = require('url')
+var dt = require('./date-time')
+
+const port = process.env.PORT || 3000
+const majorVersion = 1
+const minorVersion = 3
+
+// custom 404 page
+app.use((request, response) => {
+    response.type('text/plain')
+    response.status(404)
+    response.send('404 - Not Found')
+})
+
+// custom 500 page
+app.use((err, request, response, next) => {
+    console.error(err.message)
+    response.type('text/plain')
+    response.status(500)
+    response.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+    `Expressed Started at \"http:localhost:${port}\"\n`
+    `press Ctrl-C to terminate session.`
+))
